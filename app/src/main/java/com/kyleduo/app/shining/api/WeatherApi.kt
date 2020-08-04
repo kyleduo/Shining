099bd9ebd89168a89fa1c5f56833b0294f9524a7
kyleduo <kyleduo@gmail.com>
2020-08-04 10:33:46 +0800

    use appid & secret

--- app/src/main/java/com/kyleduo/app/shining/api/WeatherApi.kt
+++ app/src/main/java/com/kyleduo/app/shining/api/WeatherApi.kt
@@ -1,5 +1,6 @@
 package com.kyleduo.app.shining.api
 
+import com.kyleduo.app.shining.BuildConfig
 import com.kyleduo.app.shining.model.Weather
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -16,6 +17,14 @@ object WeatherApi {
 
     private fun createService(): WeatherService {
         val httpClient = OkHttpClient.Builder()
+            .addInterceptor {
+                val origin = it.request()
+                val newUrl =
+                    origin.url().newBuilder().setQueryParameter("appid", BuildConfig.API_APP_ID)
+                        .setQueryParameter("appsecret", BuildConfig.API_APP_SECRET).build()
+                val newRequest = origin.newBuilder().url(newUrl).build()
+                it.proceed(newRequest)
+            }
             .build()
 
         val retrofit: Retrofit = Retrofit.Builder()
@@ -28,7 +37,7 @@ object WeatherApi {
     }
 
     interface WeatherService {
-        @GET("api?version=v6&appid=22798437&appsecret=uena1xpw")
+        @GET("api?version=v6")
         suspend fun queryWeather(@Query("cityid") cityId: String?): Weather?
     }
 }
