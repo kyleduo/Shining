0cd7646460957ce898b2ba006a314f5e6d99392a
zhangduobj <zhangduobj@fenbi.com>
2020-11-20 20:32:36 +0800

    using mockito

--- app/src/test/java/com/kyleduo/app/shining/repos/WeatherRepositoryTest.kt
+++ app/src/test/java/com/kyleduo/app/shining/repos/WeatherRepositoryTest.kt
@@ -3,9 +3,14 @@ package com.kyleduo.app.shining.repos
 import com.kyleduo.app.shining.api.WeatherApi
 import com.kyleduo.app.shining.datastore.MemCache
 import com.kyleduo.app.shining.model.Weather
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verifyBlocking
+import com.nhaarman.mockitokotlin2.whenever
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.types.shouldBeSameInstanceAs
-import io.mockk.*
+import org.mockito.Mockito
+import org.mockito.internal.verification.VerificationModeFactory
 import java.util.*
 
 /**
@@ -13,29 +18,34 @@ import java.util.*
  */
 class WeatherRepositoryTest : StringSpec({
 
-    val weatherService: WeatherApi.WeatherService = mockk(relaxed = true)
-    val memCache: MemCache = mockk(relaxed = true)
+    val weatherService: WeatherApi.WeatherService = mock {
+        onBlocking { queryWeather(any()) }.thenReturn(null)
+    }
+    val memCache: MemCache = mock()
 
     val weatherRepository = WeatherRepository(weatherService, memCache)
 
     beforeEach {
-        clearMocks(weatherService)
-        coEvery { weatherService.queryWeather(any()) }.returns(null)
+        Mockito.clearInvocations(weatherService)
     }
 
     "When no cache, Then query through service" {
-        every { memCache.get<Any>(any()) }.returns(null)
+        whenever(memCache.get<Any>(any())).thenReturn(null)
+
         weatherRepository.queryWeather("1")
-        coVerify { weatherService.queryWeather("1") }
+        verifyBlocking(weatherService) {
+            queryWeather("1")
+        }
     }
 
     "When has cache, Then do not query service And using cache" {
         val weather = Weather(updateTime = Date())
-        every { memCache.get<Any>(any()) }.returns(weather)
+        whenever(memCache.get<Any>(any())).thenReturn(weather)
 
         val ret = weatherRepository.queryWeather("1")
-
-        coVerify(exactly = 0) { weatherService.queryWeather("1") }
+        verifyBlocking(weatherService, mode = VerificationModeFactory.noMoreInteractions()) {
+            queryWeather("1")
+        }
         weather.shouldBeSameInstanceAs(ret)
     }
 })
